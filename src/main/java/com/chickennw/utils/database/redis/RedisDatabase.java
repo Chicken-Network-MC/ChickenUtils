7ea0d849e8e197805ae8b4cdb9e509e3dbea63f2
Efe Kurban <me@efekurban.com.tr>
2025-10-04 21:01:00 +0300

    Refactor RedisDatabase and RedisMessage classes; update message handling to use String instead of JSONObject

--- src/main/java/com/chickennw/utils/database/redis/RedisDatabase.java
+++ src/main/java/com/chickennw/utils/database/redis/RedisDatabase.java
@@ -2,13 +2,14 @@ package com.chickennw.utils.database.redis;
 
 import com.chickennw.utils.ChickenUtils;
 import com.chickennw.utils.models.redis.RedisMessage;
+import com.google.gson.Gson;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.pubsub.RedisPubSubAdapter;
-import io.lettuce.core.pubsub.api.async.RedisPubSubAsyncCommands;
+import io.lettuce.core.pubsub.StatefulRedisPubSubConnection;
 import lombok.Getter;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,48 +17,65 @@ import java.util.List;
 @Getter
 public abstract class RedisDatabase {
 
-    private final RedisPubSubAsyncCommands<String, String> asyncRedisConnection;
     private final List<String> subscribedChannels = new ArrayList<>();
 
-    private RedisDatabase() {
+    private final RedisClient redisClient;
+    private final StatefulRedisConnection<String, String> redisConnection;
+    private final StatefulRedisPubSubConnection<String, String> pubSubConnection;
+
+    private final Gson gson;
+
+    public RedisDatabase() {
+        this(new Gson());
+    }
+
+    public RedisDatabase(Gson gson) {
+        this.gson = gson;
+
         FileConfiguration yaml = ChickenUtils.getPlugin().getConfig();
         String host = yaml.getString("Redis.host");
         int port = yaml.getInt("Redis.port");
         String password = yaml.getString("Redis.password");
         String user = yaml.getString("Redis.user");
 
-        RedisClient redisClient = RedisClient.create(RedisURI.Builder.redis(host, port).withAuthentication(user, password).withDatabase(1).build());
-        asyncRedisConnection = redisClient.connectPubSub().async();
+        redisClient = RedisClient.create(RedisURI.Builder.redis(host, port).withAuthentication(user, password).withDatabase(0).build());
+        pubSubConnection = redisClient.connectPubSub();
+        redisConnection = redisClient.connect();
 
         addListener();
     }
 
     public void publish(RedisMessage message) {
-        asyncRedisConnection.publish(message.channel(), message.message().toString());
+        redisConnection.async().publish(message.channel(), gson.toJson(message.message()));
     }
 
     public void subscribe(String channel) {
-        asyncRedisConnection.subscribe(channel);
+        pubSubConnection.async().subscribe(channel);
         subscribedChannels.add(channel);
     }
 
     public void unsubscribe() {
-        subscribedChannels.forEach(asyncRedisConnection::unsubscribe);
+        subscribedChannels.forEach(pubSubConnection.sync()::unsubscribe);
         subscribedChannels.clear();
     }
 
     private void addListener() {
-        asyncRedisConnection.getStatefulConnection().addListener(new RedisPubSubAdapter<>() {
+        pubSubConnection.async().getStatefulConnection().addListener(new RedisPubSubAdapter<>() {
             @Override
             public void message(String channelName, String message) {
                 if (!subscribedChannels.contains(channelName)) return;
 
-                JSONObject jsonObject = new JSONObject(message);
-                RedisMessage redisMessage = new RedisMessage(channelName, jsonObject);
+                RedisMessage redisMessage = new RedisMessage(channelName, message);
                 onMessage(redisMessage);
             }
         });
     }
 
+    public void close() {
+        unsubscribe();
+        redisClient.close();
+    }
+
     public abstract void onMessage(RedisMessage redisMessage);
+
 }
